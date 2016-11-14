950b8777974c62d5cd6ff9b3bd3325a9f74e9d0e
sabahtalateh <sabahtalateh@gmail.com>
2016-11-14 15:57:00 +0300

    WIP

---
+++ m4/1-Enums/Basics.hs
@@ -0,0 +1,60 @@
+module Basics where
+
+--import Prelude hiding (Bool, True, False)
+
+--data Bool = True | False
+--
+--alwayTrue :: Int -> Bool
+--alwayTrue n = True
+
+{-
+    Функции над типом данных определяются при помощи сопоставления с образцом
+-}
+
+data B = T | F deriving (Show, Eq, Read, Enum)
+
+not' :: B -> B
+not' T = F
+--not' F = T
+
+-- :set -fwarn-incomplete-patterns - включает и отлючает предупреждения о частичном определении
+-- :unset -fwarn-incomplete-patterns
+
+charToInt :: Char -> Int
+charToInt '0' = 0
+charToInt '1' = 1
+charToInt '2' = 2
+charToInt '3' = 3
+charToInt '4' = 4
+charToInt '5' = 5
+charToInt '6' = 6
+charToInt '7' = 7
+charToInt '8' = 8
+charToInt '9' = 9
+
+data Color = Red | Green | Blue deriving Show
+
+stringToColor :: String -> Color
+stringToColor "Red" = Red
+stringToColor "Green" = Green
+stringToColor "Blue" = Blue
+
+emptyOrSingleton :: Bool -> a -> [a]
+emptyOrSingleton False _ = []
+emptyOrSingleton True x = []
+
+isEqual :: (Eq a, Eq b) => (a, b) -> (a, b) -> Bool
+isEqual (a, b) (a', b') = True
+
+
+data LogLevel = Error | Warning | Info
+
+cmp :: LogLevel -> LogLevel -> Ordering
+cmp Error Error     = EQ
+cmp Info Info       = EQ
+cmp Warning Warning = EQ
+cmp Error _         = GT
+cmp _ Error         = LT
+cmp Info _          = LT
+cmp _ Info          = GT
+
